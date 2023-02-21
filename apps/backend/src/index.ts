224859ab4a190174a5bec751412217467811b20d
Daniel Costrasel <ohkimur@gmail.com>
2023-02-21 20:35:20 +0200

    chore(backend): add express

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, world!')
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (_req, res) => {
+  res.send('Express + TypeScript Server = 🎉')
+})
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
